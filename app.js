0ef56cae6062cd7c24f10e1cd5850b5df1181874
murayamay <murayamay@casio.co.jp>
2017-04-21 17:14:15 +0900

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,27 @@ var express = require('express');
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
+
+/***ここから追加***/
+// POSTパラメータ取得用 body-parser設定 (express4から必要)
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Date()で現在時刻を取得するためのユーティリティ
+var dateutil = require('date-utils');
+ 
+app.post('/', function(req, res){
+ var date = new Date();
+ var now = date.toFormat("YYYY/MM/DD HH24:MI:SS");
+ req.body.date = now;
+ 
+ console.log('app.js req.body: ' + JSON.stringify(req.body));
+ res.send(req.body);
+});
+/***ここまで追加***/
+
+
 // create a new express server
 var app = express();
 
